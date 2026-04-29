87e8861fc5511a75df97aecfaf7dc47cc9e04756
Jassassin1n <johmasub@gmail.com>
2026-04-29 12:07:32 +0300

    chore(ci): simplify pipeline and enable instrumentation coverage reports

--- app/src/androidTest/java/com/mxl/sandbox/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mxl/sandbox/ExampleInstrumentedTest.kt
@@ -2,23 +2,25 @@ package com.mxl.sandbox
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.mxl.sandbox", appContext.packageName)
+    }
+
+    @Test
+    fun sdk_smoke_test() {
+        println("🔥 SDK TEST EXECUTED")
+
+        // simple deterministic check to confirm execution
+        val result = 1 + 1
+        assertEquals(2, result)
     }
 }
